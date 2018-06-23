a3d206f06dfbe78eade7b42a25a55c7106e66d06
hamadavrgame <hamadavrgame@gmail.com>
2018-06-23 14:35:48 +0900

    oculusGOサンプル、コミット練習。

---
+++ Assets/my/script/myConrollerMNG.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myConrollerMNG : MonoBehaviour {
+
+    [SerializeField]
+    private Rigidbody cubeRigid;
+
+    [SerializeField]
+    private float thrust;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //スペースキー押されたら
+            cubeRigid.AddForce(transform.up * thrust, ForceMode.VelocityChange);
+        }
+
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            ////トリガー押されたら
+
+            //yellow.SetActive(false);
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.Back))
+        {
+            ///戻るボタン押されたら
+
+            //pink.SetActive(false);
+        }
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryTouchpad))
+        {
+            ////タッチパッド押されたら
+
+            //pink.SetActive(true);
+            ////pink.SetActive(false);
+            cubeRigid.AddForce(transform.up * thrust, ForceMode.VelocityChange);
+        }
+        if (OVRInput.GetDown(OVRInput.Touch.PrimaryTouchpad))
+        {
+            ///タッチパッド触れられたら
+
+        }
+    }
+}
---
+++ Assets/my/script/roll.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roll : MonoBehaviour {
+
+    [SerializeField]
+    private Vector3 vec;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(vec, Space.World);
+	}
+}
