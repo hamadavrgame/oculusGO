2f6b2e0a61a33a222de366b67696426e64039ec8
hamadavrgame <hamadavrgame@gmail.com>
2018-06-29 00:00:34 +0900

    開発版で、OculusGOでアプリ終了とアプリの再起動が正常に行う様にスプリクトを修正。シーンとtitleとSampleの２つにして軽くジャンプデモ制作。

--- Assets/my/script/myConrollerMNG.cs
+++ Assets/my/script/myConrollerMNG.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+//ゲームシーン用入力判定
 public class myConrollerMNG : MonoBehaviour {
 
     [SerializeField]
@@ -10,45 +12,78 @@ public class myConrollerMNG : MonoBehaviour {
     [SerializeField]
     private float thrust;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    [SerializeField]
+    private float roll_thrust;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        myDebug_KeyBoardInputChk();      //キーボード入力判定
+
+        oculusGoInputChk();             //oculusGO 入力判定
+
+    }
+
+
+    //キーボード入力判定
+    private void myDebug_KeyBoardInputChk()
+    {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             //スペースキー押されたら
             cubeRigid.AddForce(transform.up * thrust, ForceMode.VelocityChange);
         }
 
-
-        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        if (Input.GetKey(KeyCode.UpArrow))
         {
-            ////トリガー押されたら
+            //Zキー押されたら続けたら
 
-            //yellow.SetActive(false);
+            //回転を与える。
+            cubeRigid.AddTorque((transform.up + transform.right + transform.forward) * roll_thrust, ForceMode.Acceleration);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene(0);      //シーンID 0　へジャンプ。
+        }
+
+    }
+
+    //oculusGO 入力判定
+    private void oculusGoInputChk()
+    {
+        //if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
+        if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            ////トリガー押されている間、
+
+            //回転を与える。
+        //    cubeRigid.AddTorque((transform.up + transform.right + transform.forward) * roll_thrust, ForceMode.Acceleration);
+            cubeRigid.AddTorque((transform.up + transform.right + transform.forward) * roll_thrust, ForceMode.VelocityChange);
+
         }
 
         if (OVRInput.GetDown(OVRInput.Button.Back))
         {
             ///戻るボタン押されたら
 
-            //pink.SetActive(false);
+            SceneManager.LoadScene(0);      //シーンID 0　へジャンプ。
         }
+
         if (OVRInput.GetDown(OVRInput.Button.PrimaryTouchpad))
         {
             ////タッチパッド押されたら
 
-            //pink.SetActive(true);
-            ////pink.SetActive(false);
             cubeRigid.AddForce(transform.up * thrust, ForceMode.VelocityChange);
         }
+
         if (OVRInput.GetDown(OVRInput.Touch.PrimaryTouchpad))
         {
             ///タッチパッド触れられたら
 
         }
+
     }
+
 }
